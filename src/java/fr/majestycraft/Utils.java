262fd0dca58dd937595e47036257e317af2ee141
CapDRAKE <72074285+CapDRAKE@users.noreply.github.com>
2023-03-24 19:30:15 +0100

    patch optifine

--- src/java/fr/majestycraft/Utils.java
+++ src/java/fr/majestycraft/Utils.java
@@ -16,7 +16,7 @@ public class Utils {
 	        case "1.10.2":
 	        case "1.11.2":
 	        case "1.12.2":
-	            engine.setGameStyle(useForge ? GameStyle.FORGE_1_8_TO_1_12_2 : GameStyle.VANILLA);
+	            engine.setGameStyle(useForge ? GameStyle.FORGE_1_8_TO_1_12_2 : (useOptifine ? GameStyle.OPTIFINE : GameStyle.VANILLA));
 	            break;
 	        case "1.13.2":
 	        case "1.14.4":
@@ -25,14 +25,14 @@ public class Utils {
 	        case "1.16.3":
 	        case "1.16.4":
 	        case "1.16.5":
-	            engine.setGameStyle(useForge ? GameStyle.FORGE_1_13_HIGHER : GameStyle.VANILLA);
+	            engine.setGameStyle(useForge ? GameStyle.FORGE_1_13_HIGHER : (useOptifine ? GameStyle.OPTIFINE : GameStyle.VANILLA));
 	            break;
 	        case "1.17":
 	        case "1.17.1":
 	        case "1.18":
 	        case "1.18.1":
 	        case "1.18.2":
-	            engine.setGameStyle(useForge ? GameStyle.FORGE_1_17_HIGHER : GameStyle.VANILLA);
+	            engine.setGameStyle(useForge ? GameStyle.FORGE_1_17_HIGHER : (useOptifine ? GameStyle.OPTIFINE : GameStyle.VANILLA));
 	            break;
 	        case "1.19":
 	        case "1.19.1":
